da3a71ba6e74b135ff3f8f93fa826d4d89ff579d
NicoleZambranoS <nicole@monogram.io>
2022-09-30 14:52:47 -0500

    Clean code

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,4 @@
 import Image from "next/image";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
-
-import { Configuration, V0alpha2Api, Session, Identity } from "@ory/client";
-import { edgeConfig } from "@ory/integrations/next";
-
-const ory = new V0alpha2Api(new Configuration(edgeConfig));
-
-// Returns either the email or the username depending on the user's Identity Schema
-const getUserName = (identity: Identity) =>
-  identity.traits.email || identity.traits.username;
 
 const Footer = () => {
   return (
